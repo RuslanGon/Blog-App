5f04b479092cd0d55968cc8fa0db7ad364aa10e9
RuslanGon <ruslan441006@gmail.com>
2025-04-17 13:12:43 +0300

    wer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,8 @@ import Navbar from "./components/Navbar.jsx";
 import Register from "./components/Register.jsx";
 import Login from "./components/Login.jsx";
 import Home from "./components/Home.jsx";
+import Create from "./components/Create.jsx";
+import Contact from "./components/Contact.jsx";
 
 function App() {
   return (
@@ -13,6 +15,8 @@ function App() {
         <Route path="/" element={<Home />}></Route>
         <Route path="/register" element={<Register />}></Route>
         <Route path="/login" element={<Login />}></Route>
+        <Route path="/create" element={<Create />}></Route>
+        <Route path="/contact" element={<Contact />}></Route>
       </Routes>
     </>
   );
