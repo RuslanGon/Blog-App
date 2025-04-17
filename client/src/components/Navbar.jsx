850f0e011860d7b2bfad04f09c7fd5d5959eb051
RuslanGon <ruslan441006@gmail.com>
2025-04-17 10:02:55 +0300

    wewr

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,16 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import css from './Navbar.module.css'
 
 const Navbar = () => {
   return (
-    <div>
+    <div className={css.navbar}>
       <div>
         <h3>Blog App</h3>
       </div>
       <div>
-        <Link to='/'>Home</Link>
-        <Link to='/create'>Create</Link>
-        <Link to='/contact'>Contact</Link>
+        <Link className={css.link} to='/'>Home</Link>
+        <Link className={css.link} to='/create'>Create</Link>
+        <Link className={css.link} to='/contact'>Contact</Link>
       </div>
       <div>
         <h5>Register/Login</h5>
