b8d37b36e4b691216b424e6729a1f4b25dbcd351
RuslanGon <ruslan441006@gmail.com>
2025-04-19 12:08:21 +0300

    wer

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,13 +1,17 @@
 import axios from 'axios'
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
+import css from './Home.module.css'
 
 const Home = () => {
+
+  const [posts, setPost] = useState([])
 
   useEffect(() => {
     const fetchPosts = async () => {
       try {
         const response = await axios.get('http://localhost:3001/getposts');
         console.log(response.data);
+        setPost(response.data)
       } catch (error) {
         console.error('Ошибка при получении постов:', error);
       }
@@ -17,8 +21,21 @@ const Home = () => {
   }, []);
 
   return (
-    <div>Home</div>
-  )
+    <div>
+      <div>
+        {posts.map((post) => (
+          <div className={css.post}>
+            <img
+              src={`http://localhost:3001/Image/${post.file}`} alt={post.title}/>
+            <div className={css.post_text}>
+              <h3>{post.title}</h3>
+              <p>{post.desc}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default Home
