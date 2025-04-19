50134abd21bd3f54eade2cfac578773bc8383d8b
RuslanGon <ruslan441006@gmail.com>
2025-04-19 13:18:55 +0300

    wer

--- client/src/components/PostCard.jsx
+++ client/src/components/PostCard.jsx
@@ -1,18 +1,18 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import css from './PostCard.module.css'
+import css from './PostCard.module.css';
 
 const PostCard = () => {
-  const [post, setPost] = useState({});  
-  const { id } = useParams();  
+  const [post, setPost] = useState({});
+  const { id } = useParams();
 
   useEffect(() => {
     const fetchPostById = async () => {
       try {
         const response = await axios.get(`http://localhost:3001/getpostbyid/${id}`);
         console.log(response.data);
-        setPost(response.data);  
+        setPost(response.data);
       } catch (error) {
         console.error('Ошибка при получении поста:', error);
       }
@@ -21,11 +21,13 @@ const PostCard = () => {
   }, [id]);
 
   return (
-    <div className={css.postCard}>
-      <img src={`http://localhost:3001/Image/${post.file}`} alt={post.title} />
-      <div className={css.postCardContent}>
-        <h2>{post.title}</h2>
-        <p>{post.desc}</p>
+    <div className={css.postCardContainer}>
+      <div className={css.postCard}>
+        <img src={`http://localhost:3001/Image/${post.file}`} alt={post.title} />
+        <div className={css.postCardContent}>
+          <h2>{post.title}</h2>
+          <p>{post.desc}</p>
+        </div>
       </div>
     </div>
   );
